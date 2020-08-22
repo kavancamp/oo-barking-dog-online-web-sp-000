6490cbed79d82183ce57152c2b2b62746133b508
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-22 20:22:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,17 @@
 class Dog
   def name=(dogs_name)
     @name = dogs_name
-  end 
+  end
 
   def name
     @name
-  end 
-  
-end 
+  end
+
+end
 
 class Dog
   def bark
     puts "woof!"
-  end 
-  
+  end
+
 end 
